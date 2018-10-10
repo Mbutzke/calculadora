95a940106a8996ef95d785d882bb0c8cfb0e6c1f
Matheus Butzke <matheusbutzke99@gmail.com>
2018-10-10 10:39:14 -0400

    Hibernate

---
+++ calc/src/br/edu/ifro/LoginController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author 02119149240
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private JFXTextField txtUsuario;
+    @FXML
+    private JFXPasswordField txtSenha;
+    @FXML
+    private JFXButton btnCadastrar;
+    @FXML
+    private JFXButton btnLogin;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void login(ActionEvent event) {
+    }
+    
+}
+
+
+
