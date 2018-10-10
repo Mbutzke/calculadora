f855c2dd0701bab12aebde1e85d730288059da5c
Matheus Butzke <matheusbutzke99@gmail.com>
2018-10-10 10:54:03 -0400

    Mapeando o Banco

---
+++ calc/src/br/edu/ifro/modelo/Usuario.java
@@ -0,0 +1,43 @@
+package br.edu.ifro.modelo;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario{
+@Id 
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+private Integer id;
+private String senha;
+private String nomeUsuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+
+
+}
