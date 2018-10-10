300a758f32062c04e06ae6ed8802526c7763d7c2
Leibiniz Venâncio Chaves Lopes <lbznancio@gmail.com>
2018-10-10 10:52:56 -0400

    Calculadora básica finished

--- calc/src/br/edu/ifro/telaInicialController.java
+++ calc/src/br/edu/ifro/telaInicialController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,12 +21,54 @@ import javafx.scene.control.Label;
 public class telaInicialController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtNum1, txtNum2, txtResult;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btnSoma, btnSub, btnMult, btnDiv;
+    @FXML
+    private void Soma(ActionEvent event) {
+           Double num1 = Double.parseDouble (txtNum1.getText()); 
+           Double num2 = Double.parseDouble (txtNum2.getText()); 
+                     Double result = num1 + num2;  
+                     
+                txtResult.setText(result.toString());
+                txtNum1.setText("");
+                txtNum2.setText("");
+    }
+    
+     @FXML
+    private void Sub(ActionEvent event) {
+           Double num1 = Double.parseDouble (txtNum1.getText()); 
+           Double num2 = Double.parseDouble (txtNum2.getText()); 
+                     Double result = num1 - num2;  
+                 txtResult.setText(result.toString());
+                txtNum1.setText("");
+                txtNum2.setText("");
+    }
+    
+     @FXML
+    private void Mult(ActionEvent event) {
+           Double num1 = Double.parseDouble (txtNum1.getText()); 
+           Double num2 = Double.parseDouble (txtNum2.getText()); 
+                     Double result = num1 * num2;  
+                 txtResult.setText(result.toString());
+                  txtNum1.setText("");
+                  txtNum2.setText("");
+    }
+    
+     @FXML
+    private void Div(ActionEvent event) {
+           Double num1 = Double.parseDouble (txtNum1.getText()); 
+           Double num2 = Double.parseDouble (txtNum2.getText()); 
+                     Double result = num1 / num2;  
+                 txtResult.setText(result.toString());
+                txtNum1.setText("");
+                txtNum2.setText("");
+    }
+     @FXML
+    private void Clean(ActionEvent event) {
+                 txtResult.setText("");
+                
     }
     
     @Override
